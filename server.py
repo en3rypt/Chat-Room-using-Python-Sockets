b55d9f73a199e3f8a6ab40ee5bd86e0db213219e
en3rypt <sanjithnathan@gmail.com>
2022-02-27 10:25:24 +0530

    final commit

--- server.py
+++ server.py
@@ -12,24 +12,31 @@ ips = {}
 
 def handle_client():   
     while True:
-        
-        msg,cIp=s.recvfrom(1024)
-        if cIp not in ips:
-            ips[cIp] = msg.decode()
+        inp,cIp=s.recvfrom(1024)
+        choice,msg = inp.decode().split(":")
+        if choice == 'login':
+            for ip in ips:
+                s.sendto(bytes(f'[SERVER]: {msg} has joined the chat', 'utf-8'),ip) 
+            ips[cIp] = msg
             print("Client connected: ",cIp)
-            s.sendto(bytes("Welcome to the server",'utf-8'),cIp)
+            
+            s.sendto(bytes(f'[SERVER]: Welcome to the chat {msg}!', 'utf-8'),cIp)
             continue
-        else:
-            s.sendto(bytes("Welcome back",'utf-8'),cIp)
-            print("Message from client: ",msg.decode('utf-8'))
-        # #send msg to all clients in ips
-        # for ip in ips:
-        #     if ip != cIp:
-        #         s.sendto(msg,ip)
-
-        
-
-        
+        elif choice == 'send':
+            print(f'[{ips[cIp]}]:{msg}')
+            #send msg to all clients in ips
+            for ip in ips:
+                if ip != cIp:
+                    s.sendto(bytes(f'[{ips[cIp]}]:{msg}','utf-8'),ip)
+                else:
+                    s.sendto(bytes(f'[You]:{msg}','utf-8'),ip)
+        elif choice == 'exit':
+            print(ips)
+            del ips[cIp]
+            print(ips)
+            for ip in ips:
+                s.sendto(bytes(f'[SERVER]:{msg}','utf-8'),ip)
+                        
 def start():
     while(True):
         
--- tkinter_client.py
+++ tkinter_client.py
@@ -5,89 +5,91 @@ import threading
 from tkinter import messagebox
 
 
-from matplotlib.pyplot import fill
+from matplotlib.pyplot import fill, title
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverAddr=((socket.gethostname(),9999))
 
 
-class GUI:
+class APP:
     def __init__(self):
         self.root = Tk()
-        self.root.title("Client")
-        self.root.geometry("400x410")
+        self.root.title("Chat room")
         self.name = StringVar()
         self.msg = StringVar()
         self.login = Frame(self.root)
         self.login.pack(fill='both',expand=True)
 
-        #crteate label
-        self.name_label = Label(self.login,text="Enter your name: ")
+        self.root.eval('tk::PlaceWindow . center')
+        
+        self.welcome_label = Label(self.login,text="Welcome to the chat room",font=("Helvetica",16))
+        self.welcome_label.pack(pady=10)
+
+        self.name_label = Label(self.login,text="Enter your name: ",font=("Helvetica",12))
         self.name_label.pack()
-        #create entry
-        self.name_entry = Entry(self.login,textvariable=self.name)
+
+        self.name_entry = Entry(self.login,textvariable=self.name,font=("Helvetica",12))
         self.name_entry.pack()
-        #create button
-        self.enter_button = Button(self.login,text="Enter",command=self.validate_login)
+
+        self.enter_button = Button(self.login,text="Enter",font=("Helvetica",12),command=self.validate_login)
         self.enter_button.pack()
 
 
 
-        
-        #create mainpage frame
+
         self.mainpage = Frame(self.root)
         
 
 
         self.l1 = Label(self.mainpage,text='')
         self.l1.pack()
-        #create textbox in root
-        #create new frame
+
         self.frame = Frame(self.mainpage)
         self.frame.pack()
-        #create textbox
+
         self.textbox = Text(self.frame,height=22,width=47)
         self.textbox.pack(side='left')
-        #create scrollbar
+
         self.scrollbar = Scrollbar(self.frame)
         self.scrollbar.pack(side='right',fill='y')
-        #configure scrollbar
+
         self.scrollbar.config(command=self.textbox.yview)
         self.textbox.config(yscrollcommand=self.scrollbar.set)
         self.textbox.config(state=DISABLED)
 
-        #create new frame
         self.frame2 = Frame(self.mainpage)
         self.frame2.pack()
-        #create input left side and button right side
+
         self.input_left = Entry(self.frame2,textvariable=self.msg,width=35,font=17)
         self.input_left.pack(side='left')
         self.button_right = Button(self.frame2,text='Send',font=17,command=self.send_message)
         self.button_right.pack(side='right')
 
+        self.exit_button = Button(self.mainpage,text='Exit',font=17,command=self.logout)
+        self.exit_button.pack()
 
         self.root.mainloop()
 
+    def logout(self):
+        s.sendto(f'exit:{self.name.get()} Left the chat'.encode(),serverAddr)
+        self.root.destroy()
     def send_message(self):
-        s.sendto(self.msg.get().encode(),serverAddr)
+        s.sendto(f'send:{self.msg.get()}'.encode(),serverAddr)
         self.input_left.delete(0,END)
-        print('message sent')
             
     def get_message(self):
         
         while True:
-            print('get message')
             try:
                 s.settimeout(None)
                 reply,cIp=s.recvfrom(1024)
-                print(reply.decode())
+
                 self.textbox.config(state=NORMAL)
                 self.textbox.insert(END,reply.decode()+'\n')
                 self.textbox.config(state=DISABLED)
                 
             except:
-                print('error')
                 break
 
 
@@ -95,28 +97,23 @@ class GUI:
         name = self.name_entry.get()
         try:
             if name:
-                s.sendto(name.encode(),serverAddr)
+                s.sendto(f'login:{name}'.encode(),serverAddr)
                 s.settimeout(2)
-                reply,cIp=s.recvfrom(1024)
-                if reply:
-                    print('messsaage sent')
-                    print(reply.decode())
-                    self.login.pack_forget()
-                    self.mainpage.pack(fill='both',expand=True)
-                    get_message = threading.Thread(target=self.get_message)
-                    get_message.start()
-                    self.l1.config(text=self.name.get())
-                    print('thread started')
-                else:
-                    messagebox.showinfo('Error','Login Failed!\nTry again') 
+                self.login.pack_forget()
+                self.mainpage.pack(fill='both',expand=True)
+                get_message = threading.Thread(target=self.get_message)
+                get_message.start()
+                self.l1.config(text=self.name.get())
+                self.root.title(f'Chatroom:{self.name.get()}')
+                self.root.eval('tk::PlaceWindow . center')
+                
             else:
                 messagebox.showinfo('Error','Enter your name to continue') 
             
         except socket.timeout:
-            print('timeout')
             messagebox.showinfo('Error','Connection timed out\nServer Down')
             self.root.destroy()
             
    
 
-app = GUI()
+client = APP()
